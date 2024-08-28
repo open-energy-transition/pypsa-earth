18a04ca19ed0bc44707e02eed629586836ce066d
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-08-28 11:35:31 +0200

    resolve conflicts

--- test/conftest.py
+++ test/conftest.py
@@ -10,6 +10,7 @@ import shutil
 
 import pypsa
 import pytest
+import yaml
 
 _content_temp_file = "content"
 _name_temp_file = "hello.txt"
@@ -41,3 +42,11 @@ def get_power_network_scigrid_de():
 @pytest.fixture(scope="function")
 def get_power_network_ac_dc_meshed():
     return pypsa.examples.ac_dc_meshed(from_master=True)
+
+
+@pytest.fixture(scope="function")
+def get_config_dict():
+    path_config = pathlib.Path(pathlib.Path.cwd(), "config.default.yaml")
+    with open(path_config, "r") as file:
+        config_dict = yaml.safe_load(file)
+    return config_dict
