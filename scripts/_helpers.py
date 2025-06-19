ee018a7eb37fa1b0ea64e85b184225cf7d864bce
Daniele Lerede <daniele.lerede@openenergytransition.org>
2025-06-19 18:46:28 +0200

    run pre-commit

--- scripts/_helpers.py
+++ scripts/_helpers.py
@@ -1044,7 +1044,9 @@ def prepare_costs(
 
     # TODO: revise costs filtering
     modified_costs = modified_costs[modified_costs.scenario.isin(["Moderate", np.nan])]
-    modified_costs = modified_costs[modified_costs.financial_case.isin(["Market", np.nan])]
+    modified_costs = modified_costs[
+        modified_costs.financial_case.isin(["Market", np.nan])
+    ]
 
     # min_count=1 is important to generate NaNs which are then filled by fillna
     modified_costs = (
