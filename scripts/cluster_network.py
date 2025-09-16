c78cf23ac517e9a99cdffd2a5b1aa92bbcabd501
Daniele Lerede <daniele.lerede@openenergytransition.org>
2025-09-16 16:25:44 +0200

    run pre-commit

--- scripts/cluster_network.py
+++ scripts/cluster_network.py
@@ -739,7 +739,9 @@ if __name__ == "__main__":
             "custom_busmap", False
         )  # custom busmap is depreciated https://github.com/pypsa-meets-earth/pypsa-earth/pull/694
         if custom_busmap:
-            custom_busmap = pd.read_csv(snakemake.input.custom_busmap, index_col=0).squeeze()
+            custom_busmap = pd.read_csv(
+                snakemake.input.custom_busmap, index_col=0
+            ).squeeze()
             custom_busmap.index = custom_busmap.index.astype(str)
             logger.info(f"Imported custom busmap from {snakemake.input.custom_busmap}")
         cluster_config = snakemake.config.get("cluster_options", {}).get(
